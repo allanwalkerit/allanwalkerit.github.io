dafe49fba8cd846d24ee548c6556e32cf04e0b9e
Allan Walker <allan.walker@mapbox.com>
2019-11-17 14:02:34 -0800

    commit

---
+++ WFP/config.js
@@ -0,0 +1,173 @@
+var config = {
+    style: 'mapbox://styles/allanwalker/ck33fmbn12psu1coa0utek90s',
+    accessToken: 'pk.eyJ1IjoiYWxsYW53YWxrZXIiLCJhIjoiY2phbHVlOHQ4MnZscDMycGJoaTdiaHRxOCJ9.OYLAoMVg6e3Ih4r65WJuvA',
+    showMarkers: false,
+    theme: 'light',
+    alignment: 'left',
+    title: 'Kenya - Trends in Crop Production: A Spatial-Temporal Analysis of Production at Sub-National Level',
+    subtitle: 'While production at national level show a general increasing trend with fluctuations in particular years, available data from the Ministry of Agriculture show that the main producing areas are in the western and central regions of the country. Some of the major producing areas have recorded declining rainfall trend which may have had negative impacts on production.',
+    byline: 'Proof of Concept by Mapbox Solutions, Datablick & Starschema for World Food Programme',
+    footer: 'Source: © ICPAC/WFP 2018',
+    chapters: [
+        {
+            id: 'KenyaMaizeProduction2011',
+            title: 'Dry Maize Production in Kenya',
+            image: 'images/Kenya_Maize_Production_2011.png',
+            description: '2011',
+            location: {
+                center: [37.908000, 0.130000],
+                zoom: 5.7,
+                pitch: 0,
+                bearing: 0
+            },
+            onChapterEnter: [
+                {
+                    layer: 'kenya-counties-labels',
+                    opacity: 1
+                },
+                {
+                    layer: 'kenya-counties-line',
+                    opacity: 1
+                },
+                {
+                    layer: 'kenya-maize-production-2011',
+                    opacity: 1
+                }
+            ],
+            onChapterExit: [
+             {
+                    layer: 'kenya-counties-labels',
+                    opacity: 1
+                },
+                {
+                    layer: 'kenya-counties-line',
+                    opacity: 1
+                },
+                {
+                    layer: 'kenya-maize-production-2011',
+                    opacity: 0
+                }
+            ]
+        },
+        {
+            id: 'KenyaMaizeProduction2012',
+            title: 'Dry Maize Production in Kenya',
+            image: '',
+            description: '2012',
+            location: {
+                center: [37.908000, 0.130000],
+                zoom: 5.7,
+                pitch: 0,
+                bearing: 0
+            },
+            onChapterEnter: [
+                {
+                    layer: 'kenya-counties-labels',
+                    opacity: 1
+                },
+                {
+                    layer: 'kenya-counties-line',
+                    opacity: 1
+                },
+                {
+                    layer: 'kenya-maize-production-2012',
+                    opacity: 1
+                }
+            ],
+            onChapterExit: [
+             {
+                    layer: 'kenya-counties-labels',
+                    opacity: 1
+                },
+                {
+                    layer: 'kenya-counties-line',
+                    opacity: 1
+                },
+                {
+                    layer: 'kenya-maize-production-2012',
+                    opacity: 0
+                }
+            ]
+        },
+         {
+            id: 'KenyaMaizeProduction2013',
+            title: 'Dry Maize Production in Kenya',
+            image: '',
+            description: '2013',
+            location: {
+                center: [37.908000, 0.130000],
+                zoom: 5.7,
+                pitch: 0,
+                bearing: 0
+            },
+            onChapterEnter: [
+                {
+                    layer: 'kenya-counties-labels',
+                    opacity: 1
+                },
+                {
+                    layer: 'kenya-counties-line',
+                    opacity: 1
+                },
+                {
+                    layer: 'kenya-maize-production-2013',
+                    opacity: 1
+                }
+            ],
+            onChapterExit: [
+             {
+                    layer: 'kenya-counties-labels',
+                    opacity: 1
+                },
+                {
+                    layer: 'kenya-counties-line',
+                    opacity: 1
+                },
+                {
+                    layer: 'kenya-maize-production-2013',
+                    opacity: 0
+                }
+            ]
+        },
+         {
+            id: 'KenyaMaizeProduction2014',
+            title: 'Dry Maize Production in Kenya',
+            image: '',
+            description: '2014',
+            location: {
+                center: [37.908000, 0.130000],
+                zoom: 5.7,
+                pitch: 0,
+                bearing: 0
+            },
+            onChapterEnter: [
+                {
+                    layer: 'kenya-counties-labels',
+                    opacity: 1
+                },
+                {
+                    layer: 'kenya-counties-line',
+                    opacity: 1
+                },
+                {
+                    layer: 'kenya-maize-production-2014',
+                    opacity: 1
+                }
+            ],
+            onChapterExit: [
+             {
+                    layer: 'kenya-counties-labels',
+                    opacity: 1
+                },
+                {
+                    layer: 'kenya-counties-line',
+                    opacity: 1
+                },
+                {
+                    layer: 'kenya-maize-production-2014',
+                    opacity: 0
+                }
+            ]
+        }
+    ]
+};
