0cf24e6e628b04c3328e4e8c4d4d132dd143f13a
Allan Walker <allan.walker@mapbox.com>
2019-11-17 16:40:27 -0800

    commit

--- WFP/config.js
+++ WFP/config.js
@@ -4,16 +4,16 @@ var config = {
     showMarkers: false,
     theme: 'light',
     alignment: 'left',
-    title: 'Kenya - Trends in Crop Production: A Spatial-Temporal Analysis of Production at Sub-National Level',
-    subtitle: 'While production at national level show a general increasing trend with fluctuations in particular years, available data from the Ministry of Agriculture show that the main producing areas are in the western and central regions of the country. Some of the major producing areas have recorded declining rainfall trend which may have had negative impacts on production.',
+    title: 'Kenya - Trends in Crop Production:',
+    subtitle: 'A Spatial-Temporal Analysis of Production at Sub-National Level',
     byline: 'Proof of Concept by Mapbox Solutions, Datablick & Starschema for World Food Programme',
     footer: 'Source: © ICPAC/WFP 2018',
     chapters: [
         {
-            id: 'KenyaMaizeProduction2011',
-            title: 'Dry Maize Production in Kenya',
-            image: 'https://public.tableau.com/views/KenyaMaizeProduction2011-2014/KenyaMaizeProduction2011.png?:showVizHome=no',
-            description: '2011',
+            id: 'KenyaMaizeProductionIntro',
+            title: 'Dry Maize Production in Kenya 2011-2014',
+            image: 'https://public.tableau.com/views/KenyaMaizeProduction2011-2014/KenyaMaizeProduction2011-2014.png?:showVizHome=no',
+            description: 'While production at national level show a general increasing trend with fluctuations in particular years, available data from the Ministry of Agriculture show that the main producing areas are in the western and central regions of the country. Some of the major producing areas have recorded declining rainfall trend which may have had negative impacts on production.',
             location: {
                 center: [37.908000, 0.130000],
                 zoom: 5.7,
@@ -21,6 +21,30 @@ var config = {
                 bearing: 0
             },
 onChapterEnter: [
+  {
+      layer: 'kenya-maize-production-2011',
+      opacity: 0
+  }
+],
+onChapterExit: [
+  {
+      layer: 'kenya-maize-production-2011',
+      opacity: 0
+  }
+]
+        },
+                {
+            id: 'KenyaMaizeProduction2011',
+            title: 'Dry Maize Production in Kenya 2011',
+            image: 'https://public.tableau.com/views/KenyaMaizeProduction2011-2014/KenyaMaizeProduction2012.png?:showVizHome=no',
+            description: '',
+            location: {
+                center: [37.908000, 0.130000],
+                zoom: 5.7,
+                pitch: 0,
+                bearing: 0
+            },
+            onChapterEnter: [
   {
       layer: 'kenya-maize-production-2011',
       opacity: 1
@@ -35,9 +59,9 @@ onChapterExit: [
         },
         {
             id: 'KenyaMaizeProduction2012',
-            title: 'Dry Maize Production in Kenya',
+            title: 'Dry Maize Production in Kenya 2012',
             image: 'https://public.tableau.com/views/KenyaMaizeProduction2011-2014/KenyaMaizeProduction2012.png?:showVizHome=no',
-            description: '2012',
+            description: '',
             location: {
                 center: [37.908000, 0.130000],
                 zoom: 5.7,
@@ -59,9 +83,9 @@ onChapterExit: [
         },
          {
             id: 'KenyaMaizeProduction2013',
-            title: 'Dry Maize Production in Kenya',
+            title: 'Dry Maize Production in Kenya 2013',
             image: 'https://public.tableau.com/views/KenyaMaizeProduction2011-2014/KenyaMaizeProduction2013.png?:showVizHome=no',
-            description: '2013',
+            description: '',
             location: {
                 center: [37.908000, 0.130000],
                 zoom: 5.7,
@@ -83,9 +107,9 @@ onChapterExit: [
         },
          {
             id: 'KenyaMaizeProduction2014',
-            title: 'Dry Maize Production in Kenya',
+            title: 'Dry Maize Production in Kenya 2014',
             image: 'https://public.tableau.com/views/KenyaMaizeProduction2011-2014/KenyaMaizeProduction2014.png?:showVizHome=no',
-            description: '2014',
+            description: '',
             location: {
                 center: [37.908000, 0.130000],
                 zoom: 5.7,
