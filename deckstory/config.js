e771bdfe1c0a2b4a2eb4f71573983ed842438609
Allan Walker <allan.walker@mapbox.com>
2019-12-08 00:23:39 -0800

    commit

--- deckstory/config.js
+++ deckstory/config.js
@@ -4,16 +4,16 @@ var config = {
     showMarkers: false,
     theme: 'light',
     alignment: 'left',
-    title: '',
-    subtitle: '',
+    title: 'Statue of Liberty National Monument',
+    subtitle: 'Ellis Island and Liberty Island',
     byline: '',
-    footer: 'Sources: NOAA, NPS, USPS, Wikipedia',
+    footer: 'Sources: NOAA, NPS, Wikipedia, OpenStreetMap',
     chapters: [
         {
-            id: 'One',
+            id: 'Libery Intro',
             title: '',
             image: 'https://upload.wikimedia.org/wikipedia/commons/9/9c/Liberty_Island.jpg',
-            description: "Liberty Island is a federally owned island in Upper New York Bay in the United States. Its most notable feature is the Statue of Liberty (Liberty Enlightening the World), a large statue by Frédéric Auguste Bartholdi that was dedicated in 1886. The island is an exclave of the New York City borough of Manhattan, surrounded by the waters of Jersey City, New Jersey.<br>Long known as Bedloe's Island, it was renamed by an act of the United States Congress in 1956. Liberty Island became part of the Statue of Liberty National Monument in 1937 through Presidential Proclamation 2250, signed by President Franklin D. Roosevelt. In 1966, it was listed on the National Register of Historic Places as part of Statue of Liberty National Monument, Ellis Island and Liberty Island.",
+            description: "Liberty Island is a federally owned island in Upper New York Bay in the United States. Its most notable feature is the Statue of Liberty (Liberty Enlightening the World), a large statue by Frédéric Auguste Bartholdi that was dedicated in 1886. The island is an exclave of the New York City borough of Manhattan, surrounded by the waters of Jersey City, New Jersey.<br><br>Long known as Bedloe's Island, it was renamed by an act of the United States Congress in 1956. Liberty Island became part of the Statue of Liberty National Monument in 1937 through Presidential Proclamation 2250, signed by President Franklin D. Roosevelt. In 1966, it was listed on the National Register of Historic Places as part of Statue of Liberty National Monument, Ellis Island and Liberty Island.",
             location: {
                 center: [-74.04507, 40.68983],
                 zoom: 17,
@@ -49,8 +49,47 @@ var config = {
             onChapterExit: []
         },
         {
+            id: 'Ellis Intro',
+            title: '',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Ellis_Island_Complex.jpg/1200px-Ellis_Island_Complex.jpg',
+            description: "Ellis Island is a federally owned island in New York Harbor, within the states of New York and New Jersey, that contains a museum and former immigration inspection station of the same name. As the nation's busiest immigrant inspection station from 1892 to 1954, it processed approximately 12 million immigrants to the United States through the Port of New York and New Jersey. Today, the island is part of the Statue of Liberty National Monument, a U.S. national monument. The north side of the island hosts a museum of immigration, accessible only by ferry. The south side of the island, including the Ellis Island Immigrant Hospital, is abandoned but accessible to the public through guided tours.<br><br>In the 19th century, Ellis Island was the site of Fort Gibson, a component of the fortifications of New York Harbor. It later became a naval magazine for storing artillery. The first inspection station opened in 1892 and was destroyed by fire in 1897. The second station opened in 1900 and housed facilities for medical quarantines as well as processing immigrants. After 1924, Ellis Island was used primarily as a detention center; during both World War I and World War II its facilities were also used by the United States military. Following the immigration station's closure, the buildings languished for several years until they partially reopened in 1976. The main building and adjacent structures were completely renovated in 1990.",
+            location: {
+            center: [-74.04072, 40.69868],
+            zoom: 16.89,
+            pitch: 3.00,
+            bearing: -44.80
+            },
+            onChapterEnter: [
+    {
+      layer: 'NY2LI',
+      opacity: 0
+  },
+  {
+      layer: 'Firefly_NY2LI_1',
+      opacity: 0
+  },
+  {
+      layer: 'Firefly_NY2LI_2',
+      opacity: 0
+  },
+  {
+      layer: 'NJ2LI',
+      opacity: 0
+  },
+  {
+      layer: 'Firefly_NJ2LI_1',
+      opacity: 0
+  },
+  {
+      layer: 'Firefly_NJ2LI_2',
+      opacity: 0
+  }
+            ],
+            onChapterExit: []
+        },
+        {
             id: 'NY',
-            title: 'New York',
+            title: 'The Battery (Manhattan)',
             image: '',
             description: '',
             location: {
@@ -89,7 +128,7 @@ var config = {
         },
                 {
             id: 'NJ',
-            title: 'New Jersey',
+            title: 'Liberty State Park',
             image: '',
             description: '',
             location: {
